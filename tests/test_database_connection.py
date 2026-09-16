3708e20f1138654c39e759633c75174cb55fc1a7
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-09-16 16:19:48 +0100

    add: implement test for database connection

---
+++ tests/test_database_connection.py
@@ -0,0 +1,8 @@
+from src.database.connection import engine
+from sqlalchemy import text
+
+def test_database_connection():
+    with engine.connect() as connection:
+      
+        result = connection.execute(text("SELECT * FROM cities"))
+        print(result.fetchone())
